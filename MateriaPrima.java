94495b1ec7f2e41d6ce328846f5251a29700f5e0
Luís Gustavo Gene de Almeida <luis.gustavo.galmeida@gmail.com>
2026-09-01 18:02:41 -0300

    Criacao da classe Maquina e melhoria das outras

--- MateriaPrima.java
+++ MateriaPrima.java
@@ -1,15 +1,25 @@
 public class MateriaPrima {
-    private String id="MeBa"; //Metal base
-    private String nome="Metal";
-    private int quantidade; //int pois os produtos vão pedir apenas por int para evitar erros
-    private String unidade="metros"; // considerando chapas de metal q se compra em metro
+    private String id;
+    private String nome;
+    private int quantidade; //int pois os produtos será produzido apenas por int para evitar erros
+    private String unidade;
     private int quantidadeMinima=1; //vai depender do produto, mas é 1, pois precisar pelo menos ter em estoque pra produzir algokkkk
-    public void consumir(int demanda){
+    
+    public MateriaPrima(String ID, String nomeado, String novaUnidade, int estoque){
+        id=ID;
+        nome=nomeado;
+        unidade=novaUnidade;
+        quantidade=estoque;
+    }
+
+    public boolean consumir(int demanda){
         if (verificarDisponibilidade(demanda)){
             quantidade-=demanda; //atualiza o estoque, já consumindo o demandado
             System.out.println("Estoque atualizado!");
-        }return;
+            return true;
+        }return false;
     }
+
     public void adicionarEstoque(int encomenda){
         if(encomenda<=0){
             System.out.println("Aviso! \nValor nulo ou negativo de encomenda de MP");
@@ -19,6 +29,7 @@ public class MateriaPrima {
             System.out.println("Estoque atualizado!");
         }
     }
+
     public boolean verificarDisponibilidade(int demanda){
         if (demanda<=0){
             System.out.println("Aviso! \nDemanda nula ou negativa");//não é pra acontecer, mas por segurança existe
@@ -29,16 +40,16 @@ public class MateriaPrima {
         }
         return false;
     }
+
     public String getId(){
         return id;
     }
+
     public String getNome(){
         return nome;
     }
+
     public int getQuantidade(){
         return quantidade;
-    }
-    public String getUnidade(){
-        return unidade;
     }
 }
--- Produto.java
+++ Produto.java
@@ -1,29 +1,39 @@
 public class Produto {
-    private String id="Prod01";
-    private String nome="Capô";
+    private String id;
+    private String nome;
     private String status="parado"; //"parado", "produzido" e "avaliado"
-    private int quantidadeMateriaPrimaNecessaria=2; 
+    private int quantidadeMateriaPrimaNecessaria; 
+    
+    public Produto(String ID, String nomeado){
+        id=ID;
+        nome=nomeado;
+    }
+
     public void processar(){
         status="produzido";
-        return;
     }
+
     public void definirDemandaMateriaPrima(int demanda){
         if (demanda<=0){
             System.out.println("ERRO\nTentativa de demanda definida nula ou negativa");
         }else{
             quantidadeMateriaPrimaNecessaria=demanda;
             System.out.println("Matéria Prima necessária atualizada");
-        }return;
+        }
     }
+
     public int getDemandaMateriaPrima(){
         return quantidadeMateriaPrimaNecessaria;
     }
+
     public String getId(){
         return id;
     }
+
     public String getNome(){
         return nome;
     }
+
     public String getStatus(){
         return status;
     }
